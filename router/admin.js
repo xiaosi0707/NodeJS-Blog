0106fa76ab5f08fde72f9530b7c402122f8d47a2
wyunfei <583514368@qq.com>
2018-11-15 10:07:58 +0800

    后台首页开发

--- router/admin.js
+++ router/admin.js
@@ -4,8 +4,17 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/user', (req, res, next) => {
-    res.send('Admin')
+// router.get('/user', (req, res, next) => {
+//     console.log(req.userInfo)
+//     if(!req.userInfo.isAdmin) {
+//         res.send('对不起，只有管理员才能进入后台管理系统');
+//         return;
+//     }
+//     next();
+// })
+
+router.get('/', (req, res, next) => {
+    res.render('../views/admin/index')
 })
 
 module.exports = router
--- router/main.js
+++ router/main.js
@@ -6,7 +6,7 @@ let router = express.Router();
 
 router.get('/', (req, res, next) => {
     // res.send('首页')
-    res.render('../views/index', {
+    res.render('../views/main/index', {
         userInfo: req.userInfo // 分配模板数据
     })
 })
